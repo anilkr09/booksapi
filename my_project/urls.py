16a18e44374266a347699ef4c0c4b89ebc8a6bc2
Anil <anil121321@gmail.com>
2021-10-11 03:14:57 +0530

    commit1

--- my_project/urls.py
+++
@@ -1,9 +0,0 @@
-# URL configurations
-
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('myapp.urls'))
-]
